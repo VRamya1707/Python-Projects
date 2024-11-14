acc34b9e7a3834e59b950c2e7ae416348240744a
VRamya1707 <148433262+VRamya1707@users.noreply.github.com>
2024-11-14 08:18:51 +0530

    Add files via upload

---
+++ passwordgen.py
@@ -0,0 +1,29 @@
+# PASSWORD GENERATOR --- PROJECT
+
+import string
+import random
+
+def gen():
+    s1 = string.ascii_uppercase
+    # print(s1)
+    s2 = string.ascii_lowercase
+    # print(s2)
+    s3 = string.digits
+    # print(s3)
+    s4 = string.punctuation
+    # print(s4)
+    passlen = int(input("Enter your Password Length\n"))
+
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    # print(s)
+
+    random.shuffle(s)
+    # print(s)
+    pas = ("".join(s[0:passlen]))
+    print(pas)
+
+gen()
